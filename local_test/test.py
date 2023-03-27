1cda6543c8b46c49a3e2a119576892448e47fea8
Modjoul-William <william.rose@modjoul.com>
2023-03-27 17:18:32 -0400

    inefficient-temp-solution

--- local_test/test.py
+++ local_test/test.py
@@ -76,24 +76,28 @@ def get_ClearStream_JSON_data(BEARER_TOKEN):
     top = response.json()
     devices = top['devices']
 
+    filtered_devices = [device for device in devices if device.get('deviceid') != "C4:7D:CC:64:95:0F"]
+
     # Adds a key value pair to tell when the csv sheet was made for quicksight
-    for device in devices:
+    for device in filtered_devices:
         device['timeaccessed'] = str(timestamp)
 
-    devices_list.append(devices)
+    devices_list.append(filtered_devices)
 
     # Check to see if device is offline and if the name ends with DVA3, 
     # then add the name of the device to the offline_devices list
-    for device in devices:
+    for device in filtered_devices:
         if device['online'] == "" and device['name'].endswith('DVA3'):
             offline_devices.append(device['name'])
 
     # Check to see if devices ending in ESDC are offline
-    for device in devices:
+    for device in filtered_devices:
         if device['online'] == "" and device['name'].endswith('ESDC'):
             coles_offline_devices.append(device['name'])
-    
+
+    print(devices)
     print(coles_offline_devices)
+    print(offline_devices)
 
 def send_offline_devices_email():
     # If devices are down, send email listing which ones are
@@ -156,4 +160,4 @@ def send_offline_devices_email():
 
 
 get_ClearStream_login_token()
-send_offline_devices_email()
+# send_offline_devices_email()
